3455b517f8e8abe82af069b0207f19116a8ae7c6
RajUpadhy <rajupadhyay646464@gmail.com>
2022-06-17 14:26:24 +0530

    cool

---
+++ src/main/java/com/infy/RtpcrApplication.java
@@ -0,0 +1,14 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtpcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtpcrApplication.class, args);
+		System.out.println("FULLY_VACINATED");
+	}
+
+}
